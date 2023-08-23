e92c83bf8195a463f448771a7693f4ba69f26289
YeEun Oh <OhSolDev@gmail.com>
2023-08-24 00:46:45 +0900

    Add player horizontal moving

--- Assets/1.Scripts/ShootingMiniGame/Player/PlayerMove.cs
+++ Assets/1.Scripts/ShootingMiniGame/Player/PlayerMove.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform modelingTransform;
+    [SerializeField]
+    private float moveSpeed = 50f;
+    [SerializeField]
+    private float xRange = 2.5f;
+    private Rigidbody rigid;
+    private SphereCollider sphereCollider;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+    //-2.5 ~ 2.5
+    private void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontalInput, 0f, 0f) * moveSpeed * Time.deltaTime;
+        rigid.MovePosition(transform.position + movement);
     }
 }
