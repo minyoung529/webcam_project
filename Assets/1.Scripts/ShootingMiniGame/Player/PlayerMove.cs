c940e78148cbab221c7e068c5fcf0ed9730c6434
YeEun Oh <OhSolDev@gmail.com>
2023-08-24 12:14:49 +0900

    Add moving animation

--- Assets/1.Scripts/ShootingMiniGame/Player/PlayerMove.cs
+++ Assets/1.Scripts/ShootingMiniGame/Player/PlayerMove.cs
@@ -12,14 +12,24 @@ public class PlayerMove : MonoBehaviour
     private float xRange = 2.5f;
     private Rigidbody rigid;
     private SphereCollider sphereCollider;
+    private Animator animator;
+    int horizontal = 0, isMove = 0;
 
     private void Start()
     {
         rigid = GetComponent<Rigidbody>();
         sphereCollider = GetComponent<SphereCollider>();
+        animator = modelingTransform.GetComponent<Animator>();
+        horizontal = Animator.StringToHash("Horizontal");
+        isMove = Animator.StringToHash("IsMove");
     }
     //-2.5 ~ 2.5
     private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         if (transform.position.x < -xRange)
         {
@@ -32,5 +42,15 @@ public class PlayerMove : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(horizontalInput, 0f, 0f) * moveSpeed * Time.deltaTime;
         rigid.MovePosition(transform.position + movement);
+        animator.SetFloat(horizontal, horizontalInput);
+        if (horizontalInput > -0.1f && horizontalInput < 0.1f)
+        {
+            animator.SetBool(isMove, false);
+        }
+        else
+        {
+            animator.SetBool(isMove, true);
+        }
+
     }
 }
