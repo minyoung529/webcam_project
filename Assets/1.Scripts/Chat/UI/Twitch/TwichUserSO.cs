755bdc32f28fe043f7da344177d5f9d42ea89b7c
YeEun Oh <OhSolDev@gmail.com>
2023-09-11 20:25:51 +0900

    Add twich chatting ui and create scritps

---
+++ Assets/1.Scripts/Chat/UI/Twitch/TwichUserSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="TwichUserSO",menuName ="Data/Twitch/UserSO")]
+public class TwichUserSO : ScriptableObject
+{
+    
+}
